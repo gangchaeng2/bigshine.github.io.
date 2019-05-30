95ccd47811ecf816a3dac9f297d9625573b90bdf
강태욱 <twkang@station3.co.kr>
2019-05-30 18:42:15 +0900

    폰트 제거

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -32,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
     height: 100%;
     margin: 0;
     padding: 0;
-    font-family: 'BMHANNAAir', 'Sans-serif'; 
+    ${'' /* font-family: 'BMHANNAAir', 'Sans-serif';  */}
     box-sizing: border-box;
   }
 
